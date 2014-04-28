f43c2823e283b678dea7e1d1b6b19805a61c3dfa
jordi <jordiruiz@iesmontsia.org>
2014-04-28 17:58:16 +0200

    Capçalera adaptada a Bootstrap

--- Escoles2.0/index.php
+++ Escoles2.0/index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+    <title>Escoles 2.0</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all" />
+    <script src="js/jquery-2.1.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('dropdown-menu').find('form').click(function(e) {
+                    e.stopPropagation();
+            });
+            $('.fotos').carousel();
+        });
+    </script>
 <html>
     <head>
         <meta charset="UTF-8">
