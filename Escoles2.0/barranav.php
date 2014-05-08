9e8113817c0f445c42b7ce0de2b5c9f3884d2d02
jordi <jordi@jordi-VGN-NR21E-S>
2014-05-08 17:57:13 +0200

    Arreglo barra superior

---
+++ Escoles2.0/barranav.php
@@ -0,0 +1,30 @@
+<div role="navigation" class="navbar navbar-inverse navbar-static-top">
+ <div class="navbar-inner">		
+    <div class="container">
+       <div class="navbar-header">
+            <button type="button" data-target="#navbar-main" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Menú</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand">Escoles 2.0</a>
+        </div>
+    <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+             <?php
+             require_once 'classes/connexio.php';
+             $db=new connexio();
+
+             $sql = $db->query("SELECT Nom,Cognom1,Cognom2 FROM Usuaris WHERE ID = ".$_SESSION['id']);
+             $resultat = $sql->fetch_array(MYSQLI_ASSOC);
+             $db->close() ?>
+            <p class="navbar-text"><?php echo $resultat['Nom'].' '.$resultat['Cognom1'].' '.$resultat['Cognom2'] ?></p>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="logout.php">Sortir</a></li>
+          </ul>
+    </div>
+    </div>
+ </div>
+</div>
